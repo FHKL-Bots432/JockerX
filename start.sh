95edbd14860d510a23c9f736aa609c5877ce0fb7
FHKL-Bots432 <96911602+FHKL-Bots432@users.noreply.github.com>
2022-01-21 19:53:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/PR0FESS0R-99/DonLee-Robot-V2.git /DonLee-Robot-V2
+  git clone https://github.com/FHKL-Bots432/JockerX.git /JockerX
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DonLee-Robot-V2
+  git clone $UPSTREAM_REPO /JockerX
 fi
-cd /DonLee-Robot-V2
+cd /JockerX
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
